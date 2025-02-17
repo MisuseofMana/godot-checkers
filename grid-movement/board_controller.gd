b2ead8c50063b60860666c16e3b7df6b58b54285
Sean Yager <contact@seanyager.com>
2025-02-17 12:34:20 -0500

    youtube aseprite, event gate for buttons

--- grid-movement/board_controller.gd
+++ grid-movement/board_controller.gd
@@ -3,9 +3,10 @@ extends TileMapLayer
 signal valid_move_clicked(where)
 
 func _input(event):
-	if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-		var clickedCell: Vector2i = local_to_map(to_local(event.position))
-		var data = get_cell_tile_data(clickedCell)
-		if data:
-			if data.get_custom_data("validMove"):
-				valid_move_clicked.emit(clickedCell)
+	if event is InputEventMouse:
+		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+			var clickedCell: Vector2i = local_to_map(to_local(event.position))
+			var data = get_cell_tile_data(clickedCell)
+			if data:
+				if data.get_custom_data("validMove"):
+					valid_move_clicked.emit(clickedCell)
